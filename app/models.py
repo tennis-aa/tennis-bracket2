2c45dc553e0a4db34d707d823a86b7e0119ec362
tennis-aa <andresalban94@gmail.com>
2022-01-02 11:29:06 -0500

    Fix dabase connection

--- app/models.py
+++ app/models.py
@@ -12,6 +12,8 @@ def setup_db(app):
     database_name ='tennis_bracket'
     default_database_path= "postgresql://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', database_name)
     database_path = os.getenv('DATABASE_URL', default_database_path)
+    if database_path.startswith("postgres://"):
+        database_path = database_path.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
